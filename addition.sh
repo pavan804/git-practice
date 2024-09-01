7b9665c857fb2076f0b397bc1210ae1c68184b84
raghavarajur <122515407+raghavarajur@users.noreply.github.com>
2024-09-01 12:45:16 +0530

    commiting the addition script

--- addition.sh
+++ addition.sh
@@ -2,5 +2,17 @@
 NUM1=$1
 NUM2=$2
 
+if [ $# -lt 2]
+ then
+     echo "Please the 2 numbers for addition"
+     exit 1
+fi
+
+         
+
+ 
+      
+  
+
 SUM=$(($NUM1 + $NUM2))
 echo "Sum of the numbers $NUM1,$NUM2 is $SUM"
